2dbe803b8334b9eac046d33dcbfd698ad00a9fa2
abdulahad0110 <abdulahad200325@gmail.com>
2024-03-13 15:46:02 +0500

    JS 03

---
+++ script.js
@@ -0,0 +1,22 @@
+// question 1
+var userAge = prompt("Enter your Age : ");
+alert("i am " + userAge + " years old");
+
+// question 2
+var websiteVisitor = 15;
+alert("You have visited this site " + websiteVisitor + " times");
+
+// question 3
+var birthYear = 2006;
+document.write("My brith year is " + birthYear);
+document.write("<br>");
+document.write("Data type of my variable is integer");
+
+// Question 4
+var storeName = ""
+var visitorName = "Jhon wick";
+var productTitle = "T-shrit";
+var productQuantity = 6;
+document.write("<br>");
+document.write("<br>");
+document.write(visitorName + " ordered " + productQuantity + " " + productTitle + "(s) on .");
